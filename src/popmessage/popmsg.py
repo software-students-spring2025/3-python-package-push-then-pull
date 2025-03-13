967d19549c9ebe4305561111060de9c489cf2ee3
jessicahc <jhc10024@nyu.edu>
2025-03-13 00:25:25 -0400

    Reorganized code into PopupMessage class, removed font style

--- src/popmessage/popmsg.py
+++ src/popmessage/popmsg.py
@@ -3,48 +3,69 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
-# Global variables
-message = "Default Message"
-bkgColor = "white"       # Can be either color name or (R,G,B,A) tuple
-fontColor = "black"      # Can be either color name or (R,G,B,A) tuple
-font = "Arial"
-fontSize = 100
-timerDuration = 0
+class PopupMessage(App):
+    def __init__(self):
+        super().__init__()
+        # Set the default properties of the pop-up message window
+        self._setProperties("Default Message", "white", "black", 75, 0)
 
-# Set all properties of the pop-up message, will use default values if not set
-def setProperties(msg="Default Message", bColor="white", fColor="black", ft="Arial", ftSize=100, duration=0):
-    global message, bkgColor, fontColor, font, fontSize, timerDuration
-    message = msg
-    bkgColor = bColor
-    fontColor = fColor
-    font = ft
-    fontSize = ftSize
-    timerDuration = duration
+    def _setProperties(self, msg, bgColor, fontColor, fontSize, timerDuration):
+        """
+        Protected method: Sets all pop-up message window properties
+        """
+        self._message = msg
+        self._bgColor = bgColor
+        self._fontColor = fontColor
+        self._fontSize = fontSize
+        self._timerDuration = timerDuration
 
-# Create the pop-up message using the configured properties
-def createPopup():
-    global message, bkgColor, fontColor, font, fontSize, timerDuration
     
-    # Make sure all properties are set before displaying the pop-up message
-    if None in {message, bkgColor, fontColor, font, fontSize, timerDuration}:
-        raise ValueError("Error: Please set the properties before displaying the message.")
+    def _createPopup(self):
+        """
+        Protected method: Create the pop-up message window using the configured properties
+        """
+        Window.clearcolor = self._bgColor  
+        layout = BoxLayout(orientation='vertical')
+        label = Label(text=self._message, color=self._fontColor, font_size=self._fontSize)
+        
+        layout.add_widget(label)
+        return layout
     
-    Window.clearcolor = bkgColor  
-    layout = BoxLayout(orientation='vertical')
-    label = Label(text=message, color=fontColor, font_name=font, font_size=fontSize)
-    
-    layout.add_widget(label)
-    return layout
+    def displayPopup(self, msg=None, bgColor=None, fontColor=None, fontSize=None, timerDuration=None):
+        """
+        Display the pop-up message window with the specified or default properties.
+        """
+        # Use default values if parameters are not provided
+        if msg is None:
+            msg = self._message
+        if bgColor is None:
+            bgColor = self._bgColor
+        if fontColor is None:
+            fontColor = self._fontColor
+        if fontSize is None:
+            fontSize = self._fontSize
+        if timerDuration is None:
+            timerDuration = self._timerDuration
+        
+        self._setProperties(msg, bgColor, fontColor, fontSize, timerDuration)
+        self.run()
 
-class MyPopup(App):
     def build(self):
-        return createPopup()
-
-# Display the pop-up message window with the configured properties
-def displayPopup():
-    MyPopup().run()
+        """
+        Kivy's abstract method that every child class must implement 
+        and gets automatically invoked after App.run() is called.
+        """ 
+        return self._createPopup()
 
 if __name__ == '__main__':
-    # Example popup window
-    setProperties("Hello, World!", "purple", (1, 0, 0, 1), "Times New Roman", 100)
-    displayPopup()
+    # Example1 default pop-up message window
+    myPopup1 = PopupMessage()
+    myPopup1.displayPopup()
+
+    # Example2 user defined pop-up message window
+    myPopup2 = PopupMessage()
+    myPopup2.displayPopup(msg="Hello World", bgColor="blue")
+
+    # Example3 user defined pop-up message window
+    myPopup3 = PopupMessage()
+    myPopup3.displayPopup(msg="foobar", bgColor="magenta", fontColor="purple")
