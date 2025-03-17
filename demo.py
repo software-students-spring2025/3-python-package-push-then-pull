3763d99e594e1438bc73b98c069c1eb3a55f82e6
Catherine1342 <catherinehuang114@gmail.com>
2025-03-17 14:14:22 -0400

    uncommented examples 1-4 in demo.py

--- demo.py
+++ demo.py
@@ -6,23 +6,23 @@ Run this file to execute example code putting our package into practice
 
 '''
 # Example1 default pop-up message window
-#myPopup1 = PopupMessage()
-#myPopup1.displayPopup()
+myPopup1 = PopupMessage()
+myPopup1.displayPopup()
 
 # Example2 user defined pop-up message window
-#myPopup2 = PopupMessage()
-#myPopup2.displayPopup(msg="Hello World", bgColor="blue")
+myPopup2 = PopupMessage()
+myPopup2.displayPopup(msg="Hello World", bgColor="blue")
 
 # Example3 user defined pop-up message window
-#myPopup3 = PopupMessage()
-#myPopup3.displayPopup(msg="foobar", bgColor="magenta", fontColor="purple")
+myPopup3 = PopupMessage()
+myPopup3.displayPopup(msg="foobar", bgColor="magenta", fontColor="purple")
 
 #Example4 debug succes/error message test
-#myPopup4 = PopupMessage()
-#myPopup4.displaySFPopup(lambda: print("Code ran successfully!"))
-#myPopup4.displaySFPopup(lambda: 5 + 5)
-#myPopup4.displaySFPopup(lambda: print(hello))
-#myPopup4.displaySFPopup(lambda: 1 / 0)
+myPopup4 = PopupMessage()
+myPopup4.displaySFPopup(lambda: print("Code ran successfully!"))
+myPopup4.displaySFPopup(lambda: 5 + 5)
+myPopup4.displaySFPopup(lambda: print(hello))
+myPopup4.displaySFPopup(lambda: 1 / 0)
 
 #Example5 random pop-up message window
 myPopup5 = PopupMessage()
