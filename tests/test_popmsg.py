228ca9e1668e612571a47fa9248d48d63c936f73
Catherine1342 <catherinehuang114@gmail.com>
2025-03-18 16:28:57 -0400

    added unit test to check sound loader for random function

--- tests/test_popmsg.py
+++ tests/test_popmsg.py
@@ -160,3 +160,18 @@ class Tests:
         assert 30 <= fontSize <= 80
         assert timerDuration == 0
         mock_run.assert_called_once()
+    
+    # Test random popup sound
+    def test_random_popup_sound(self, mocker, popup):
+        sound = mocker.Mock()
+        mock_sound_loader = mocker.patch("popmessage.popmsg.SoundLoader.load", return_value = sound)
+        mocker_set_properties = mocker.patch.object(popup, "_setProperties", wraps = popup._setProperties)
+        mock_run = mocker.patch.object(popup, "run")
+        popup.displayRandomPopup()
+
+        # Check if sound was loaded
+        sound_args = mock_sound_loader.call_args[0][0]
+        assert sound_args in popup.sounds
+
+        sound.play.assert_called_once()
+        mock_run.assert_called_once()
