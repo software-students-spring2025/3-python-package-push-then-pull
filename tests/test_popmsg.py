7bd2b585a22667c1ded0db987a2276c03d358097
jessicahc <jhc10024@nyu.edu>
2025-03-17 22:59:59 -0400

    removed redundant create popup functions and related static vars

--- tests/test_popmsg.py
+++ tests/test_popmsg.py
@@ -21,7 +21,6 @@ class Tests:
         assert actual == expected, "Expected True to be equal to True!"
 
     def test_initializations(self, popup):
-        assert popup._type == PopupMessage.POPUP_TYPE_REGULAR
         assert popup._message == "Default Message"
         assert popup._bgColor == "white"
         assert popup._fontColor == "black"
@@ -36,27 +35,11 @@ class Tests:
         assert popup._fontSize == 50
         assert popup._timerDuration == 10
     
+   
     def test_build(self, mocker: MockerFixture, popup):
-        mock_create_popup = mocker.patch.object(popup, "_createPopup", return_value="Regular Popup")
-        mock_create_timer_popup = mocker.patch.object(popup, "_createTimerPopup", return_value="Timer Popup")
-        mock_create_sf_popup = mocker.patch.object(popup, "_createSFPopup", return_value="Success/Fail Popup")
-        mock_create_random_popup = mocker.patch.object(popup, "_createRandomPopup", return_value="Random Popup")
-
-        popup._type = PopupMessage.POPUP_TYPE_REGULAR
-        assert popup.build() == "Regular Popup"
+        mock_create_popup = mocker.patch.object(popup, "_createPopup", return_value="Popup Created")
+        assert popup.build() == "Popup Created"
         mock_create_popup.assert_called_once()
-
-        popup._type = PopupMessage.POPUP_TYPE_TIMER
-        assert popup.build() == "Timer Popup"
-        mock_create_timer_popup.assert_called_once()
-
-        popup._type = PopupMessage.POPUP_TYPE_SUCCESS_FAIL
-        assert popup.build() == "Success/Fail Popup"
-        mock_create_sf_popup.assert_called_once()
-
-        popup._type = PopupMessage.POPUP_TYPE_RANDOM
-        assert popup.build() == "Random Popup"
-        mock_create_random_popup.assert_called_once()
 
     def test_create_popup(self, popup):
         popup._bgColor = [1, 1, 1, 1]
@@ -74,7 +57,6 @@ class Tests:
         mock_run = mocker.patch.object(popup, "run")
 
         popup.displayPopup()
-        assert popup._type == PopupMessage.POPUP_TYPE_REGULAR
 
         # Check that _setProperties was called with default values
         mock_set_properties.assert_called_once_with(
@@ -95,7 +77,6 @@ class Tests:
         timerDuration = 7.5
 
         popup.displayPopup(msg, bgColor, fontColor, fontSize, timerDuration)
-        assert popup._type == PopupMessage.POPUP_TYPE_REGULAR
         
         # Check that _setProperties was called with custom values
         mock_set_properties.assert_called_once_with(msg, bgColor, fontColor, fontSize, timerDuration)
